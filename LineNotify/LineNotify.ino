bc1e3a3410e2324394b536f315b05fcd2dcbcb0a
Pakpoom Iamsouyngam <jcubuntu@Pakpooms-iMac.local>
2018-03-31 23:26:08 +0700

    Add ESP8266 Line Notify

---
+++ LineNotify/LineNotify.ino
@@ -0,0 +1,71 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#define LINE_TOKEN "YOUR LINE TOKEN"
+#define MESSAGE "YOUR MESSAGE"
+
+const char* ssid     = "SSID";
+const char* password = "PASSWORD";
+
+void Line_Notify(String LINE_Token, String message) {
+
+  String msg = String("message=") + message;
+
+  WiFiClientSecure client;
+  if (!client.connect("notify-api.line.me", 443)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String req = "";
+  req += "POST /api/notify HTTP/1.1\r\n";
+  req += "Host: notify-api.line.me\r\n";
+  req += "Content-Type: application/x-www-form-urlencoded\r\n";
+  req += "Authorization: Bearer " + String(LINE_Token) + "\r\n";
+  req += "Content-Length: " + String(msg.length()) + "\r\n";
+  req += "\r\n";
+  req +=  msg;
+
+  client.print(req);
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if ((WiFi.status() == WL_CONNECTED)) {
+    Line_Notify(LINE_TOKEN, MESSAGE);
+    delay(50000);
+  }
+}
